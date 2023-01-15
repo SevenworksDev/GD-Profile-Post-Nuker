e77de7425acae8f558f94f854272cd8411db68ae
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-01-14 21:03:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ console.log("Profile Post Nuker by Sevenworks")
 const unval = prompt("Username: ")
 const pwval = prompt("Password: ")
 const comval = prompt("Message: ")
+console.clear()
+console.log("Started the nuke! Check profile posts!")
 
 gd.users.login({ username: unval, password: pwval })
   .then(lolpog => setInterval(() => lolpog.postAccountComment(comval), 1200));
